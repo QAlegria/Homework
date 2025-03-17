f5fc6691d1163affc17dcdcf7351e6017f667629
QAlegria <test.space.job@gmail.com>
2025-03-17 18:31:42 +0700

    Homework_1, fixed print and added into main

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from math_utils.statistics import statistics_avg, statistics_med
 user_list = list(map(int, input('Введите числа через пробел: ').split()))
 print(user_list)
 
-statistics_avg(user_list)
-statistics_med(user_list)
-geometry_circle(2)
-geometry_rectangle(2, 3)
+avg = statistics_avg(user_list)
+med = statistics_med(user_list)
+s_circle = geometry_circle(2)
+s_rectangle = geometry_rectangle(2, 3)
+
+
+print(f'Среднее арифметическое = {avg}')
+print(f'Медиана = {med}')
+print(f'Площадь круга равна = {s_circle}')
+print(f'Площадь прямоугольника равна = {s_rectangle}')
--- math_utils/statistics.py
+++ math_utils/statistics.py
@@ -1,6 +1,5 @@
 def statistics_avg(param_list: list) -> int:
     avg = sum(param_list)/(len(param_list))
-    print(f'Среднее арифметическое = {avg}')
     return avg
 
 def statistics_med(param_list: list) -> int:
@@ -10,5 +9,4 @@ def statistics_med(param_list: list) -> int:
         med = (param_list[(len_list//2)-1] + param_list[len_list//2]) / 2
     else:
         med = param_list[len_list//2]
-    print(f'Медиана = {med}')
     return med
