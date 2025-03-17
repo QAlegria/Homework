82eac10150ad666c6e27a7ec037eda942cb37eb9
QAlegria <test.space.job@gmail.com>
2025-03-17 18:25:13 +0700

    Homework_1, fixed main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import math_utils
+from math_utils.geometry import geometry_circle, geometry_rectangle
+from math_utils.statistics import statistics_avg, statistics_med
 
 user_list = list(map(int, input('Введите числа через пробел: ').split()))
 print(user_list)
 
-math_utils.statistics_avg(user_list)
-math_utils.statistics_med(user_list)
-math_utils.geometry_circle(2)
-math_utils.geometry_rectangle(2, 3)
+statistics_avg(user_list)
+statistics_med(user_list)
+geometry_circle(2)
+geometry_rectangle(2, 3)
